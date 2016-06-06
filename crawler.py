c2c9ee36b348610a33f04c18d3abbd880e6ca99b
arsh23 <programmer.arsh@gmail.com>
2016-06-07 01:23:34 +0530

    improvements to functions

--- crawler.py
+++ crawler.py
@@ -11,14 +11,17 @@ import gevent
 
 startTime = time.time()
 
-
-
 class HtmlItem():
 
-    def __init__(self,url):
+    def __init__(self,id,url):
+        self.id = id
+        self.parent_id = 0
         self.url = url
         self.title = ''
-        self.links = []
+        # self.links = []
+        # self.possible_links = []
+        # self.html_code = ''
+        self.recursion_level = 0
 
     def __repr__(self):
         pass
@@ -36,11 +39,17 @@ class RecursiveCrawler():
         self.session = requests.Session()
         self.start_url = start_url
         self.pages_processsed = 0
+        self.requests_sent = 0
+        self.id_count = 0
+        self.max_recursion_level = 2
+        # self.items = []
 
-        self.root = HtmlItem(start_url)
-        #parse this
+        self.root = HtmlItem(0,start_url)
+        #parse root
         self.unfinished_links_queue.put_nowait(self.root)
         greenlet_thread = self.workers_pool.spawn(self.add_new_links)
+        self.id_count += 1
+        # self.items.append(self.root)
         self.workers_pool.start(greenlet_thread)
         self.workers_pool.join()
 
@@ -50,46 +59,41 @@ class RecursiveCrawler():
     def __str__(self):
         pass
 
-    def request_html(self):
-        tempitem = self.unfinished_links_queue.get_nowait()
-        url = tempitem.url
-        print 'Request sent for - ',url
-
+    def request_html(self,url):
+        print 'Request sent for - '+url
+        self.requests_sent +=1
         try:
             response = self.session.get(url)
         except Exception as e:
-            print 'Request for '+url+' failed, will try later'
-            self.unfinished_links_queue.put_nowait(tempitem)
+            print 'Request for '+url+' failed, will try later : ',e.message
             return None
-
         if response.status_code == 200:
             return response.text
         else:
             return None
 
-    def parse(self,html_code):
+    def parse(self):
+        tempitem = self.unfinished_links_queue.get_nowait()
+        url = tempitem.url
+
+        html_code = self.request_html(url)
+        if html_code is None:
+            return []
+
         tree = html.fromstring(html_code)
         try:
             title = tree.xpath('//*[@id="firstHeading"]/text()')[0]
         except Exception as e:
-            print 'Error parsing'
+            print 'Error parsing title : ',e.message
+            self.unfinished_links_queue.put_nowait(tempitem)
             return []
 
-
-        links = tree.xpath('//*[@id="mw-content-text"]//a')
-
-        next_links = []
-        for link in links:
-            try:
-                next_links.append(link.xpath('.//@href')[0])
-            except Exception as e:
-                pass # add something here
-
-
-        print 'Parsed - ', title
-        self.pages_processsed += 1
-
-        return next_links
+        print 'Parsed - ', title, 'pro',self.pages_processsed,'req',self.requests_sent
+        try:
+            return [ (tempitem, x.xpath('.//@href')[0]) for x in tree.xpath('//*[@id="mw-content-text"]//a') ]
+        except Exception as e:
+            print 'Error parsing <a> tags : ',e.message
+            pass # add something here
 
     def check_url(self,url):
         if url[0:6] == '/wiki/' and url[-4:-3] != '.':
@@ -97,17 +101,22 @@ class RecursiveCrawler():
         return 'bad'
 
     def add_new_links(self):
-        valid_links = [ self.domain + url for url in self.parse(self.request_html()) if self.check_url(url) == 'good' ]
+        valid_links = [ (x[0],self.domain + x[1]) for x in self.parse() if self.check_url(x[1]) == 'good' ]
+
         for link in valid_links:
-            tempitem = HtmlItem(link)
-            self.unfinished_links_queue.put_nowait(tempitem)
+            self.pages_processsed += 1
+            tempitem = HtmlItem(self.id_count,link[1])
+            self.id_count += 1
+            tempitem.parent_id = link[0].id
+            if link[0].recursion_level + 1 < self.max_recursion_level:
+                tempitem.recursion_level = link[0].recursion_level + 1
+                self.unfinished_links_queue.put_nowait(tempitem)
 
     def crawl(self):
         while not self.unfinished_links_queue.empty() and not self.workers_pool.full():
             for x in xrange(0, min(self.unfinished_links_queue.qsize(), self.workers_pool.free_count())):
                 greenlet_thread = self.workers_pool.spawn(self.add_new_links)
                 self.workers_pool.start(greenlet_thread)
-                # print 'queue',q.qsize(),'pool',p.free_count()
 
         self.workers_pool.join()
 
